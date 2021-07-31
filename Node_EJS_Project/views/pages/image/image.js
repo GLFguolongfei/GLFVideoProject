e382da4ef25afcd2ab141acf080f7e640c48e4a8
longfeifengwu <15618632831@163.com>
2021-07-31 20:49:11 +0800

    目录结构更改

---
+++ Node_EJS_Project/views/pages/image/image.js
@@ -0,0 +1,148 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 变量 */
+var sourceType = $('.sourceType').text();
+var ipUrl = $('.ipUrl').text();
+var documentTitle = '图片'
+
+var initIndex = 0;
+var currentIndex = 0; 
+var pageSize = 30;
+var dataArray = [];
+var currentEle = "";
+var timer; // 圣诞雪花计时器
+var itemHeight = '60vh'
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 加载完成 */
+$(function () {
+    $('.modal').hide()
+    currentIndex = getQueryString('index') || 0
+    currentIndex = parseInt(currentIndex)
+    initIndex = parseInt(currentIndex)
+    // 加载资源
+    var array = document.getElementsByClassName("source");
+    for (var i = 0; i < array.length; i++) {
+        var ele = array[i];
+        var src = ipUrl + ele.textContent;
+        dataArray.push(src);
+    }
+    $('.info').remove()
+    if (currentIndex > dataArray.length - 1) {
+        alert('下标已超出视频资源数量, 默认从0开始')
+        currentIndex = 0
+        initIndex = 0
+    }
+    addMore();
+    // 设置标题
+    documentTitle = '图片(' + array.length + ')'
+    document.title = documentTitle
+});
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 方法 */
+function addMore(type = 1) {
+    if (type == 2 && dataArray.length - currentIndex > 500) {
+        addMore()
+        setTimeout(function() {
+            alert('图片太多，不建议一次性加载全部')
+        }, 1000)
+        return
+    }
+    if (currentIndex - initIndex > 500) {
+        var url = window.location.origin + window.location.pathname + '?index=' + currentIndex
+        // window.location.replace(url)
+        window.location.href = url
+        return
+    }
+    $(".addMore").remove();
+    var html = '';
+    if (type == 1) {
+        currentIndex += pageSize;
+        if (dataArray.length < currentIndex) {
+            for (var i = currentIndex-pageSize; i < dataArray.length; i++) {
+                html += '<img class="item" style="height:' + itemHeight + '" src="' + dataArray[i] + '" onclick="itemClick(this)" />';
+            } 
+        } else {
+            for (var i = currentIndex-pageSize; i < currentIndex; i++) {
+                html += '<img class="item" style="height:' + itemHeight + '" src="' + dataArray[i] + '" onclick="itemClick(this)" />';
+            } 
+            html += '<div class="addMore">'         
+            html += '<button onclick="addMore(1)">点击加载更多......</button>';
+            html += '<button onclick="addMore(2)">点击加载全部......</button>';
+            html += '</div>' 
+        }
+    } else {
+        for (var i = currentIndex; i < dataArray.length; i++) {
+            html += '<img class="item" style="height:' + itemHeight + '" src="' + dataArray[i] + '" onclick="itemClick(this)" />';
+        } 
+        currentIndex = dataArray.length - 1
+    }
+    $(".container").append(html);
+}
+
+function playAudio() {
+    timer = snowFlow({
+        num: $(window).width() / 35,
+        text: "❄"
+    });
+    $(".snow").show()
+
+    var html = '';
+    for (var i = 0; i < dataArray.length; i++) {
+        html += '<img class="item marqueeItem" style="height:' + itemHeight + '" src="' + dataArray[i] + '" onclick="itemClick(this)" />';
+    } 
+    $(".marquee").append(html);
+}
+
+function pauseAudio() {
+    clearInterval(timer)
+    $(".snow").hide()
+    $(".marqueeItem").remove();
+}
+  
+function itemClick(self) {
+    $('#showImg').attr('src', self.src)
+    if (currentEle == self) {
+        $('.modal').hide()
+    } else {
+        $('.modal').show()
+    }
+}
+
+function handleModal() {
+    $('.modal').hide()
+}
+
+function imgBig() {
+    var array = document.getElementsByClassName("item");
+    for (var i = 0; i < array.length; i++) {
+        var img = array[i];
+        let heightStr = img.style.height || '65vh'
+        let height = parseInt(heightStr.substr(0, 2)) 
+        if (height + 10 < 100) {
+            itemHeight = height + 10 + 'vh'
+            img.style.height = itemHeight
+        } 
+    }
+}
+
+function imgSmall() {
+    var array = document.getElementsByClassName("item");
+    for (var i = 0; i < array.length; i++) {
+        var img = array[i];
+        let heightStr = img.style.height || '65vh'
+        let height = parseInt(heightStr.substr(0, 2)) 
+        if (height - 10 > 10) {
+            itemHeight = height - 10 + 'vh'
+            img.style.height = itemHeight
+        }
+    }
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 工具 */
+// 采用正则表达式获取地址栏参数
+// alert(getQueryString("password"));
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r!=null)return  decodeURI(r[2]); return null;
+}
+
+
---
+++ Node_EJS_Project/views/pages/videoDY/videoDY.js
@@ -0,0 +1,147 @@
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 变量 */
+var sourceType = $('.sourceType').text();
+var ipUrl = $('.ipUrl').text();
+var documentTitle = '视频'
+
+var currentIndex = 0; 
+var dataArray = [];
+var isCircul = false // 是否自动
+var isFullScreen = false // 是否全屏
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 加载完成 */
+$(function () {
+    // 加载资源
+    var array = document.getElementsByClassName("source");
+    for (var i = 0; i < array.length; i++) {
+        var ele = array[i];
+        var src = ipUrl + ele.textContent;
+        dataArray.push(src);
+    }
+    $('.info').remove()
+
+    let title = window.localStorage.getItem('title')
+    dataArray.map((item, index) => {
+        var pathArray = item.split('/');
+        var name = pathArray[pathArray.length-1];
+        if (name == title) {
+            currentIndex = index
+        }
+    })
+
+    let videoSrc = dataArray[currentIndex]
+    $('#video').attr('src', videoSrc)
+
+    addNames()
+
+    // 设置标题
+    documentTitle = '视频(' + array.length + ')'
+    document.title = documentTitle
+});
+
+// 监听键盘事件
+$(document).keydown(function(event){
+    if (event.keyCode == 13) { // enter
+        console.log('你按下了Enter'); 
+    } else if (event.keyCode == 32) { // backspace
+        // video移除焦点,否则放大的同时会暂停播放
+        $('#video').blur();
+        if (isFullScreen) {
+            $('#video').removeClass('videoFull')
+        } else {
+            $('#video').addClass('videoFull')
+        }
+        isFullScreen = !isFullScreen
+    } else if (event.keyCode == 38) { // arrow up
+        preVideo()
+    } else if (event.keyCode == 40) { // arrow down
+        nextVideo()
+    } else {
+        // alert(event.keyCode)
+    }
+});
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 方法 */
+function addNames() {
+    let html = ''
+    for (let i = 0; i < dataArray.length; i++) {
+        let path = dataArray[i]
+        let array = path.split('/')
+        if (i == currentIndex) {
+            html += '<p id="nameSelect" onclick="select(' + i + ')">【' + (i + 1) + '】' + array[array.length - 1] + '</p>' 
+        } else {
+            html += '<p onclick="select(' + i + ')">【' + (i + 1) + '】' + array[array.length - 1] + '</p>' 
+        }
+    }
+    $('#names').html(html);
+
+    let currentEle = document.getElementById('nameSelect')
+    currentEle.scrollIntoViewIfNeeded()
+}
+
+function playVideo(self) {
+    console.log('开始', self.id)
+    var model = dataArray[currentIndex];
+    var pathArray = model.split('/');
+    var name = pathArray[pathArray.length-1];
+    var title = name.split('.')[0];
+    // 设置标题
+    document.title =  (currentIndex + 1) + '/' + dataArray.length + '-' + title;
+    // 保存本地
+    window.localStorage.setItem('title', name)
+}
+
+function endVideo(self) {
+    console.log('结束', self.id)
+    currentIndex++
+    console.log(currentIndex)
+    let videoSrc = dataArray[currentIndex]
+    $('#video').attr('src', videoSrc)
+    addNames()
+}
+
+function videoCircul() {
+    isCircul = !isCircul;
+    var videoCircul = document.getElementById("videoCircul");
+    if (isCircul) {
+        videoCircul.src = 'views/images/circulSelect.png'
+        $('#video').attr('loop', null)
+    } else {
+        videoCircul.src = 'views/images/circul.png'
+        $('#video').attr('loop', '')
+    }
+}
+
+// 上一首
+function preVideo() {
+    currentIndex--
+    let videoSrc = dataArray[currentIndex]
+    $('#video').attr('src', videoSrc)
+    addNames()
+}
+
+// 下一首
+function nextVideo() {
+    currentIndex++
+    let videoSrc = dataArray[currentIndex]
+    $('#video').attr('src', videoSrc)
+    addNames()
+}
+
+// 选择播放
+function select(index) {
+    currentIndex = index
+    let videoSrc = dataArray[currentIndex]
+    $('#video').attr('src', videoSrc)
+    addNames()
+}
+
+/* ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 工具 */
+// 采用正则表达式获取地址栏参数
+// alert(getQueryString("password"));
+function getQueryString(name) {
+    var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+    var r = window.location.search.substr(1).match(reg);
+    if(r!=null)return  decodeURI(r[2]); return null;
+}
+
+
