076563291c3cc2a212b637eceef9929785af1ad2
longfeifengwu <15618632831@163.com>
2021-07-31 21:10:52 +0800

    优化

--- Node_EJS_Project/views/pages/index.js
+++ Node_EJS_Project/views/pages/index.js
@@ -1,13 +1,17 @@
 
-// 返回 0 ~ 1 之间的随机数
-let i = Math.random()
-// 把数四舍五入为最接近的整数
-i = Math.round(i * 10)
-console.log(i)
-if (i >0 && i < 10) {
+
+let i = Math.random() // 返回 0 ~ 1 之间的随机数
+i = Math.round(i * 10) // 把数四舍五入为最接近的整数
+if (i > 0 && i < 10) {
     document.body.style.background = 'white url(views/images/bg' + i + '.jpeg) no-repeat fixed'
-    document.body.style.backgroundSize = 'cover'
-    document.body.style.backgroundPosition = 'center'
 }
 
+let messge = `注意
+    本项目所用到的知识点
+    1.Node.js
+    2.Node.js的EJS模块引擎
+    3.Node.js的http静态服务器
+    使用本地资源访问视频 , 请使用Chrome浏览器 , Safari浏览器有问题（原因未知）
+`
+console.log(messge)
 
